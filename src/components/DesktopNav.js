6e488d54873e10fbda1808243d87e6403d2acf23
System Administrator <root@Ronits-MacBook.local>
2024-06-29 14:56:29 +0530

    Meta data aaded

--- src/components/DesktopNav.js
+++ src/components/DesktopNav.js
@@ -1,12 +1,16 @@
 "use client"
 import { ModeToggle } from "@/components/ModeToggle";
 import NavLinks from "./NavLinks";
+import Image from 'next/image'
+
 
 export default function DesktopNav() {
   return (
     <>
     <div className="desktop-nav-container border-b-2">
-        <div className="logo">Logo</div>
+        <div className="logo">
+        <Image src="/harmony_logo.png" width={200} height={20} alt="site-logo" />
+        </div>
         <div className="links"><NavLinks/></div>
         <div className="buttons"><ModeToggle /></div>
     </div>
